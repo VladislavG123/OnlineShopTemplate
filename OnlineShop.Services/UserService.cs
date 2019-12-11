1a6aac9532a3721666a81b8999bd95c420c5d582
Горохов Владислав Олегович <@corp.itstep>
2019-12-11 18:38:39 +0600

    first commit

---
+++ OnlineShop.Services/UserService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using OnlineShop.DataAccess;
+using OnlineShop.Options;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Services
+{
+    public class UserService
+    {
+        private readonly OnlineShopContext context;
+        private readonly string jwtSecret;
+
+        public UserService(OnlineShopContext context, IOptions<SecretOptions> secretOptions)
+        {
+            this.context = context;
+            this.jwtSecret = secretOptions.Value.JWTSecret;
+        }
+
+        public async Task<string> Authenticate(string phoneNumber)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(user => user.PhoneNumber == phoneNumber);
+
+            if (user is null) return null;
+
+            //https://jasonwatmore.com/post/2019/10/11/aspnet-core-3-jwt-authentication-tutorial-with-example-api
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jwtSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.MobilePhone, phoneNumber)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
+        }
+
+        public async Task<string> Registrate(string fullName, string phoneNumber)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(user => user.PhoneNumber == phoneNumber);
+
+            if (!(user is null)) return null;
+
+            await context.Users.AddAsync(new Domain.User
+            {
+                PhoneNumber = phoneNumber,
+                Cart = new Domain.Cart(),
+                FullName = fullName,
+                FavoriteProducts = new List<Domain.FavoriteProduct>()
+            });
+
+            await context.SaveChangesAsync();
+
+            //https://jasonwatmore.com/post/2019/10/11/aspnet-core-3-jwt-authentication-tutorial-with-example-api
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(jwtSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.MobilePhone, phoneNumber)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
+        }
+    }
+}
---
+++ OnlineShop/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Services;
+
+namespace OnlineShop.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService userService;
+
+        public AuthController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Auth(string phoneNumber)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var token = await userService.Authenticate(phoneNumber);
+
+            if (String.IsNullOrEmpty(token))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { token });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registrate(string fullName, string phoneNumber)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var token = await userService.Registrate(fullName, phoneNumber);
+
+            if (String.IsNullOrEmpty(token))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { token });
+        }
+    }
+}
