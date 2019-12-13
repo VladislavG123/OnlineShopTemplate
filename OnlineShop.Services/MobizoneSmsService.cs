d255a4ecb63fe1bc579b5e9b6a075cfc6b790fca
Владислав Горохов <gbg.0101@mail.ru>
2019-12-13 16:29:42 +0600

    fix phone format

--- OnlineShop.Services/MobizoneSmsService.cs
+++ OnlineShop.Services/MobizoneSmsService.cs
@@ -12,6 +12,7 @@ namespace OnlineShop.Services
     {
         public void SendVerificationCode(string phoneNumber, string code)
         {
+            phoneNumber.TrimStart('+');
             string url = $"https://api.mobizon.kz/service/message/sendsmsmessage?" +
                 $"recipient={phoneNumber}&text=Code: {code}" +
                 $"&apiKey=kz739b92e1907f9680a0b71e3851ab59dcec2c26af77d8ee39876b18483fa5b232126f";
